c950b700b8909a8d7b9b3b29acccd13b64b505c2
Antonio A. Russo <aarusso@nyxk.com.br>
2023-05-16 17:09:14 -0300

    OnNew

--- chats/amplify/backend/function/onNewMessage/src/index.js
+++ chats/amplify/backend/function/onNewMessage/src/index.js
@@ -1,7 +1,9 @@
+const axios = require('axios');
+
 const { DynamoDBClient, UpdateItemCommand } = require("@aws-sdk/client-dynamodb");
 const client = new DynamoDBClient();
 
-const update = async (topic, field, increment) => {
+const updateDB = async (topic, field, increment) => {
   const params = {
     TableName: 'Topic-uy3cshk2f5clhle3gk7dynqgtq-staging',
     Key: { "id": { S: topic } },
@@ -13,6 +15,46 @@ const update = async (topic, field, increment) => {
   const command = new UpdateItemCommand(params);
   return client.send(command);
 };
+
+const updateQL = async (topic, field, increment) => {
+  let n;
+
+
+  if (field === 'n_subs') {
+    // get topics(topic).n_subs from dynamodb
+  } else if (field === 'n_msgs') {
+    // get topics(topic).n_msgs from dynamodb
+  }
+    
+
+
+
+  const gqlMutation = `
+  mutation UpdateTopic($id: ID!, $n_subs: Int!, $n_msgs: Int!) {
+    updateTopic(input: { id: $id, n_subs: $n_subs, n_msgs: $n_msgs }) {
+      id
+      n_subs
+      n_msgs
+    }
+  }
+  `;
+
+  const appSyncApiUrl = 'https://your-appsync-api-endpoint/graphql'; // Replace with your AppSync API endpoint URL
+
+  return axios.post(appSyncApiUrl, {
+    query: gqlMutation,
+    variables: {
+      id: topic,
+      [field]: n+increment,
+    },
+  });
+
+
+
+
+}
+
+
 
 exports.handler = event => {
   const ups = event.Records
@@ -28,7 +70,7 @@ exports.handler = event => {
         }[img.__typename.S];
       };
 
-      return update ( topic, field(image), delta );
+      return updateDB ( topic, field(image), delta );
     });
 
   return Promise.all(ups);
